76697c5b1a94c4cd811b144835e92e0c719c1d14
Alexandr Kurganov <alexandr.kurs.it@gmail.com>
2024-09-28 15:54:26 +0600

    Replaced the icon, cleaned up the code and updated the styles.

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,15 @@ interface Product {
 }
 
 interface Props {
-    key: string;
     product: Product;
     onClick: React.MouseEventHandler;
     cartItems: CartItem[];
     removeFromCart: (id: string) => void;
+}
+
+interface CartItem {
+    id: string;
+    qty: number;
 }
 
 const PRODUCTS: Product[] = [
@@ -32,19 +36,19 @@ const PRODUCTS: Product[] = [
 ];
 
 const ProductCard: React.FC<Props> = ({product, onClick}) => {
-  return (
-    <div className="card" style={{width:'350px', cursor: 'pointer'}} onClick={onClick}>
-        <div className="card-body row">
-            <div className="col">
-                <img className="image" src={product.icon} alt={product.title + 'image'}/>
-            </div>
-            <div className="col-auto">
-                <h5>{product.title}</h5>
-                <p>Prive: {product.price} KGS</p>
+    return (
+        <div className="card cardwidth" onClick={onClick}>
+            <div className="card-body row">
+                <div className="col">
+                    <img className="image" src={product.icon} alt={product.title + 'image'}/>
+                </div>
+                <div className="col-auto">
+                    <h5>{product.title}</h5>
+                    <p>Prive: {product.price} KGS</p>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 };
 
 const initialState: CartItem[] = PRODUCTS.map(product => {
@@ -54,100 +58,96 @@ const initialState: CartItem[] = PRODUCTS.map(product => {
     }
 });
 
-interface CartItem {
-    id: string;
-    qty: number;
-}
-
 const Cart: React.FC<Props> = ({cartItems, removeFromCart}) => {
-  const totalQty = cartItems.reduce((total, item) => total + item.qty, 0);
+    const totalQty = cartItems.reduce((total, item) => total + item.qty, 0);
 
-  const displayCartItems = cartItems.filter(item => item.qty > 0).map((cartItem) => {
-    const product = PRODUCTS.find(p => p.id === cartItem.id);
-    if (!product) {
-        throw new Error ('Product not found.');
-    }
-    return {
-        id: cartItem.id,
-        product,
-        qty: cartItem.qty,
-        price: cartItem.qty * product.price,
-    }
-  });
+    const displayCartItems = cartItems.filter(item => item.qty > 0).map((cartItem) => {
+        const product = PRODUCTS.find(p => p.id === cartItem.id);
+        if (!product) {
+            throw new Error ('Product not found.');
+        }
+        return {
+            id: cartItem.id,
+            product,
+            qty: cartItem.qty,
+            price: cartItem.qty * product.price,
+        }
+    });
 
-  const totalPrice = displayCartItems.reduce((total, item) => total + item.price, 0);
+const totalPrice = displayCartItems.reduce((total, item) => total + item.price, 0);
 
-  return (
-    <div className="card">
-        <div className="card-body">
-            {totalQty === 0 ? (
-                <div>Order is empty!<br/>Please add some items!</div>
-            ) : (
-                <>
-                    {displayCartItems.map((item) => (
-                        <div key={item.id} className="row">
-                            <div className="col">{item.product.title}</div>
-                            <div className="col-auto">x {item.qty}<strong> {item.price} KGS</strong></div>
-                            <div className="col-auto">
-                                <button onClick={() => removeFromCart(item.id)}>X</button>
+    return (
+        <div className="card">
+            <div className="card-body">
+                {totalQty === 0 ? (
+                    <div>Order is empty!<br/>Please add some items!</div>
+                ) : (
+                    <>
+                        {displayCartItems.map((item) => (
+                            <div key={item.id} className="row m-3">
+                                <div className="col">{item.product.title}</div>
+                                <div className="col-2">x {item.qty}</div>
+                                <div className="col-3"><strong>{item.price} KGS</strong></div>
+                                <div className="col-auto">
+                                    <button onClick={() => removeFromCart(item.id)}>X</button>
+                                </div>
                             </div>
+                        ))}
+                        <div className="m-4">
+                            Total Price: <strong>{totalPrice} KGS</strong>
                         </div>
-                    ))}
-                    <div>
-                        Total Price: <strong>{totalPrice} KGS</strong>
-                    </div>
-                </>
-            )}
+                    </>
+                )}
 
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 const App = () => {
 
     const [cartItems, setCartItems] = useState<CartItem[]>(initialState);
 
-  const addProductToCart = (id:string) => {
-      setCartItems((prevState) => {
-          return prevState.map(cartItem => {
-              if (cartItem.id === id) {
-                  return {...cartItem, qty: cartItem.qty + 1};
-              }
-              return cartItem;
-          })
-      });
-  };
+    const addProductToCart = (id:string) => {
+        setCartItems((prevState) => {
+            return prevState.map(cartItem => {
+                if (cartItem.id === id) {
+                    return {...cartItem, qty: cartItem.qty + 1};
+                }
+                return cartItem;
+            })
+        });
+    };
 
-  const removeProductFromCart = (id: string) => {
-      setCartItems((prevState ) => {
-          return prevState.map(cartItem => {
-              if (cartItem.id === id) {
-                  return {...cartItem, qty: 0};
-              }
-              return cartItem;
-          })
-      })
-  };
+    const removeProductFromCart = (id: string) => {
+        setCartItems((prevState ) => {
+            return prevState.map(cartItem => {
+                if (cartItem.id === id) {
+                    return {...cartItem, qty: 0};
+                }
+                return cartItem;
+            })
+        })
+    };
 
-  return (
-    <div className="container-fluid mt-4">
-        <div className="row">
-            <div className="col-6">
-                <Cart cartItems = {cartItems} removeFromCart={removeProductFromCart}/>
-            </div>
-            <div className="col-6">
-                <div className="card">
-                    <div className="card-body d-flex flex-wrap gap-2">
-                        {PRODUCTS.map(product => (
-                            <ProductCard key={product.id} product={product} onClick={() => addProductToCart(product.id)} />
-                        ))}
+    return (
+        <div className="container-fluid mt-4">
+            <div className="row">
+                <div className="col-6">
+                    <Cart cartItems = {cartItems} removeFromCart={removeProductFromCart}/>
+                </div>
+                <div className="col-6">
+                    <div className="card">
+                        <div className="card-body d-flex flex-wrap gap-2">
+                            {PRODUCTS.map(product => (
+                                <ProductCard key={product.id} product={product} onClick={() => addProductToCart(product.id)} />
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 };
 
 export default App;
